2ddaf593ead1a7da53dee23ac40275a6db70bed6
Ahmad MHD Yones <ahmadmhdyones@gmail.com>
2021-08-04 09:04:52 +0300

    misc.

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,32 @@
 
 using namespace std;
 
-#define SIZE(v)  ((int)((v).size()))
-#define ALL(x) 	 x.begin(), x.end()
-#define RALL(x)  x.rbegin(), x.rend()
-#define SORT(x)  sort(ALL(x))
-#define READ(x)  freopen(x, "r", stdin)
-#define WRITE(x) freopen(x, "w", stdout)
-#define DCM(a) 	 cout << setprecision(a) << fixed
-#define PP(x) 	 ">>> " << #x << " : " << x << endl
-#define DD(x) 	 clog << PP(x)
+// #define endl '\n'
+#define SIZE(v) ((int)((v).size()))
+#define ALL(x) x.begin(), x.end()
+#define RALL(x) x.rbegin(), x.rend()
+#define SORT(x) sort(ALL(x))
+#define DCM(a) cout << setprecision(a) << fixed
+#define PP(x) ">>> " << #x << " : " << x << endl
+#define DD(x) clog << PP(x)
 
 typedef long long ll;
 typedef long double ld;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
 typedef unsigned int ui;
+typedef pair<int, int> ii;
 typedef unsigned long long ul;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef vector<double> vd;
 
-ld const PI = acos(-1.0L); //3.1415926535897932385
-ll const MOD = 1e9 + 7;    //1000000007
-ld const EPS = (1e-7);     //0.0000001
-ll const N = 200005;       //(2 * 10^5) + 5
+ld const PI = acos(-1.0L); 	//3.1415926535897932385
+ll const MOD = 1e9 + 7;    	//1,000,000,007
+ld const EPS = (1e-7);     	//0.0000001
+ll const N = 200005;       	//(2 * 10^5) + 5
+ll const OO = 1e8;			//100,000,000
 
 ll gcd(ll a, ll b) { return (b == 0 ? a : gcd(b, a % b)); }
 ll lcm(ll a, ll b) { return (a * b / gcd(a, b)); }
@@ -41,11 +44,21 @@ int solve()
 
 int main()
 {
-	ios::sync_with_stdio(NULL); cout.tie(NULL); cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	// freopen("in.txt", "r", stdin);
+	// freopen("out.txt", "w", stdout);
+#endif
+
+	ios::sync_with_stdio(NULL);
+	cout.tie(NULL);
+	cin.tie(NULL);
 
 	int t = 1;
-	// cin >> t;
-	while (t--) solve();
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
 
 	return 0;
 }

155344ce500e42b0e013eb58d5fa5cf45f7596c2
Ahmad MHD Yones <ahmadmhdyones@gmail.com>
2022-01-09 20:02:29 +0200

    misc

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,13 @@
+/******************************
+
+	@author:    amy
+	@title:     (◔◡◔)
+	@date:      Thu__18_Nov_2021
+
+******************************/
 #include <bits/stdc++.h>
 
 using namespace std;
-
-// #define endl '\n'
-#define SIZE(v) ((int)((v).size()))
-#define ALL(x) x.begin(), x.end()
-#define RALL(x) x.rbegin(), x.rend()
-#define SORT(x) sort(ALL(x))
-#define DCM(a) cout << setprecision(a) << fixed
-#define PP(x) ">>> " << #x << " : " << x << endl
-#define DD(x) clog << PP(x)
-
-typedef long long ll;
-typedef long double ld;
-typedef unsigned int ui;
-typedef pair<int, int> ii;
-typedef unsigned long long ul;
-typedef vector<ll> vl;
-typedef vector<int> vi;
-typedef vector<ii> vii;
-typedef vector<vi> vvi;
-typedef vector<string> vs;
-typedef vector<double> vd;
-
-ld const PI = acos(-1.0L);	//3.1415926535897932385
-ll const MOD = 1e9 + 7;		//1,000,000,007
-ld const EPS = (1e-7);		//0.0000001
-ll const N = 200005;		//(2 * 10^5) + 5
-ll const OO = 1e8;			//100,000,000
-
-ll gcd(ll a, ll b) { return (b == 0 ? a : gcd(b, a % b)); }
-ll lcm(ll a, ll b) { return (a * b / gcd(a, b)); }
-ll max(ll a, ll b) { return (a > b ? a : b); }
-ll min(ll a, ll b) { return (a < b ? a : b); }
-ll power(ll a, ll b, ll m = MOD) { return (b == 0 ? 1 : (b == 1 ? a : (b % 2 ? ((((power(a, b / 2, m) * power(a, b / 2, m)) % m) * a) % m) : (((power(a, b / 2, m) * power(a, b / 2, m)) % m) % m)))); }
 
 int solve()
 {
@@ -44,27 +18,17 @@ int solve()
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-	// freopen("in.txt", "r", stdin);
-	// freopen("out.txt", "w", stdout);
-#endif
-
-	ios::sync_with_stdio(NULL);
-	cout.tie(NULL);
-	cin.tie(NULL);
-
-	int t = 1;
-	// cin >> t;
+	int t;
+	t = 1;
+	cin >> t;
 	while (t--)
-	{
 		solve();
-	}
 
 	return 0;
 }
 
 /* Time Stamp:
-- Start
+- Start	Thu__18_Nov_2021___08_33_32
 - Read
 - Think
 - Code

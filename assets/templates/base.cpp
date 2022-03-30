d128d05130121719a0952150535133d71b6b968b
amy <ahmadmhdyones@gmail.com>
2022-03-30 08:31:50 +0300

    Simple changes

--- assets/templates/base.cpp
+++ assets/templates/base.cpp
@@ -1,7 +1,7 @@
 /******************************
-    @author:    amy
-    @title:     The Monster is BACK! (◔◡◔)
-    @date:      Day__DD_Mon_YYYY
+@author:    amy
+@title:     The Monster is BACK! (◔◡◔)
+@date:      Day__DD_Mon_YYYY
 ******************************/
 #include <bits/stdc++.h>
 
@@ -16,16 +16,16 @@ int solve()
 int main()
 {
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
         solve();
     return 0;
 }
 
 /* Time Stamp:
-- Start Thu__25_Mar_2021___15_41_21
-- Read  Thu__25_Mar_2021___15_43_39
-- Think Thu__25_Mar_2021___15_54_21
-- Code  Thu__25_Mar_2021___15_59_15
-- Debug Thu__25_Mar_2021___16_17_57
+- Start Day__DD_Mon_YYYY
+- Read  Day__DD_Mon_YYYY
+- Think Day__DD_Mon_YYYY
+- Code  Day__DD_Mon_YYYY
+- Debug Day__DD_Mon_YYYY
 */

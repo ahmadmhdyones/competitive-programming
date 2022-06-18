848c81737e2e15d204716c87f8286dd56f1b5e85
Ahmad Yones <ahmadmhdyones@gmail.com>
2022-06-18 04:43:03 +0300

    Add misc file

---
+++ assets/misc/misc.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string convert_decimal_to_base(long long decimal, long long base)
+{
+    string res = "";
+    while (decimal) {
+        res = (char)((decimal % base) + '0') + res;
+        decimal /= base;
+    }
+    return res;
+}
+
+
+long long convert_decimal_to_base(long long decimal, long long base)
+{
+    long long res = 0, x = 1;
+    while (decimal) {
+        res += (decimal % base) * x;
+        decimal /= base;
+        x *= 10;
+    }
+    return res;
+}

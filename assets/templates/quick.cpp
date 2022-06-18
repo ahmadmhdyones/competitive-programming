2b3b9ab6799ac40fb348776002cea826241762df
Ahmad Yones <ahmadmhdyones@gmail.com>
2022-06-18 04:42:19 +0300

    Add new templates files

--- assets/templates/quick.cpp
+++ assets/templates/quick.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define PP(x) ">>> " << #x << " : " << x << endl
+#define DD(x) clog << PP(x)
+typedef long long ll;
+ll const MOD = 1e9 + 7;
+ll const N = 200005;
+ll gcd(ll a, ll b) { return (b == 0 ? a : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { return (a * b / gcd(a, b)); }
+ll max(ll a, ll b) { return (a > b ? a : b); }
+ll min(ll a, ll b) { return (a < b ? a : b); }
+ll power(ll a, ll b, ll m = MOD) { return (b == 0 ? 1 : (b == 1 ? a : (b % 2 ? ((((power(a, b / 2, m) * power(a, b / 2, m)) % m) * a) % m) : (((power(a, b / 2, m) * power(a, b / 2, m)) % m) % m)))); }
+
 int solve()
 {
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	int t = 1;
-	cin >> t;
-	while (t--) solve();
-	return 0;
+    ios::sync_with_stdio(NULL), cout.tie(NULL), cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
 }
